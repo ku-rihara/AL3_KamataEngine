8ca8d3a85d1c5f427f63bf4172604674a5a6a65f
ku-rihara <k023g0027@g.neec.ac.jp>
2024-06-13 12:29:55 +0900

    Matrix関数

---
+++ DirectXGame/Geometry/Vector3.cpp
@@ -0,0 +1,132 @@
+#include "Vector3.h"
+#include <math.h>
+
+const float kColumnWidth = 60;
+
+Vector3 Vector3::operator+(const Vector3& obj) const {
+
+	Vector3 result;
+
+	result.x = x + obj.x;
+	result.y = y + obj.y;
+	result.z = z + obj.z;
+
+	return result;
+}
+
+Vector3 Vector3::operator-(const Vector3& obj) const {
+
+	Vector3 result;
+
+	result.x = x - obj.x;
+	result.y = y - obj.y;
+	result.z = z - obj.z;
+
+	return result;
+}
+
+Vector3 Vector3::operator*(const Vector3& obj) const {
+
+	Vector3 result;
+
+	result.x = x * obj.x;
+	result.y = y * obj.y;
+	result.z = z * obj.z;
+
+	return result;
+}
+
+Vector3 Vector3::operator*(const float& obj) const {
+	Vector3 result;
+
+	result.x = x * obj;
+	result.y = y * obj;
+	result.z = z * obj;
+
+	return result;
+}
+
+Vector3 Vector3::operator/(const Vector3& obj) const {
+
+	Vector3 result;
+
+	if (obj.x != 0) {
+		result.x = x / obj.x;
+
+	} else {
+		result.x = 0;
+	}
+
+	if (obj.y != 0) {
+		result.y = y / obj.y;
+
+	} else {
+		result.y = 0;
+	}
+
+	if (obj.z != 0) {
+		result.z = z / obj.z;
+
+	} else {
+		result.z = 0;
+	}
+
+	return result;
+}
+
+void Vector3::operator+=(const Vector3& obj) {
+
+	x += obj.x;
+	y += obj.y;
+	z += obj.z;
+	
+}
+
+void Vector3::operator+=(const float& obj) {
+
+	x += obj;
+	y += obj;
+	z += obj;
+}
+
+void Vector3::operator-=(const Vector3& obj) {
+
+	x -= obj.x;
+	y -= obj.y;
+	z -= obj.z;
+}
+
+
+
+// 内積
+float Dot(const Vector3& v1, const Vector3& v2) { return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z; }
+
+// 長さ（ノルム）
+float Length(const Vector3& v) { return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z); }
+
+// 正規化
+Vector3 Normnalize(const Vector3& v) {
+	Vector3 result;
+
+	float length = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+	if (length != 0) {
+		result.x = v.x / length;
+		result.y = v.y / length;
+		result.z = v.z / length;
+	} else {
+		result.x = 0;
+		result.y = 0;
+		result.z = 0;
+	}
+	return result;
+}
+
+Vector3 Cross(const Vector3& v1, const Vector3& v2) {
+	Vector3 result;
+
+	result.x = (v1.y * v2.z) - (v1.z * v2.y);
+	result.y = (v1.z * v2.x) - (v1.x * v2.z);
+	result.z = (v1.x * v2.y) - (v1.y * v2.x);
+	return result;
+}
+
---
+++ DirectXGame/Geometry/fMatrix4x4.h
@@ -0,0 +1,37 @@
+#pragma once
+#include"Matrix4x4.h"
+#include"Vector3.h"
+
+
+Matrix4x4 MakeIdentity4x4();
+
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+
+Vector3 Multiply(const Vector3& vector, const Matrix4x4& matrix);
+
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+
+Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+
+// X軸回転行列
+Matrix4x4 MakeRotateXMatrix(float radian);
+
+// Y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float radian);
+
+// Z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float radian);
+
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
+
+Vector3 TransformNormal(const Vector3& v, const Matrix4x4& m);
+
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3 translate);
+
+Matrix4x4 Inverse(const Matrix4x4& m);
+
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip);
+
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
+
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
